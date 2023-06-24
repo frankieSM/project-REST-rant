897dc4f9b3c1af4453fa5e9e3b35e8b7a27fae97
franc <fmenendezwork@gmail.com>
2023-06-23 22:24:33 -0700

    Adding places controller

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express()
 
+//IMPORT PLACES.JS (The first argument to app.use, /places sets all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.)
+app.use('/places', require('./controllers.places'))
+
 //HOME ROUTE
 app.get('/', (req, res)=>{
     res.send('hello world')
