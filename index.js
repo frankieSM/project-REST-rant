80354645375bfb2ef317f1efd6140a41da6882fb
franc <fmenendezwork@gmail.com>
2023-06-27 16:47:56 -0700

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ require('dotenv').config()
 const express = require('express');
 const app = express()
 
+
+
 //DEFINES THE VIEW ENGINE
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+//LET EXPRESS KNOW WHAT STATIC FOLDER IS CALLED
+app.use(express.static('public'));
 
 //IMPORT PLACES.JS (The first argument to app.use, /places sets all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.)
 app.use('/places', require('./controllers/places'))
