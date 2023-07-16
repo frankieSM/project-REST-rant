3e83250140948820655327f4c5b68f785006c220
franc <fmenendezwork@gmail.com>
2023-07-15 17:50:02 -0700

    mongoose added to show route. mongoose npm not yet installed

--- controllers/places.js
+++ controllers/places.js
@@ -30,9 +30,18 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+//SHOW SPECIFIC PLACE ROUTE
 router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
