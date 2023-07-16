258b2f6bc2f4fcaaf28280251c68db6e49bc2421
franc <fmenendezwork@gmail.com>
2023-07-16 12:43:35 -0700

    show.jsx new formatting. images not working.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,32 +4,41 @@ const Def = require("../default");
 function show(data) {
   return (
     <Def>
-      <main>
-        <h1>{data.place.name}</h1>
-        <div>
-          <h2>Rating</h2>
-          <p>Currently Unrated</p>
-        </div>
-        <div>
-          <h2>Description</h2>
-          <p>
-            Located in {data.place.city} , {data.place.state} and serving{" "}
-            {data.place.cuisines}
-          </p>
-        </div>
-        <div>
-          <h2>Comments</h2>
-          <p>No comments yet!</p>
-        </div>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
-      </main>
+<main>
+  <div class="row">
+    <div class="col-md-6">
+      <img src={data.place.pic} alt={data.place.name} />
+      <h3>
+        Located in {data.place.city}, {data.place.state}
+      </h3>
+    </div>
+    <div class="col-md-6">
+      <h1>{data.place.name}</h1>
+      <div>
+        <h2>Rating</h2>
+        <p>Currently Unrated</p>
+      </div>
+      <div>
+        <h2>Description</h2>
+        <h3>{data.place.showEstablished()}</h3>
+        <h4>Serving {data.place.cuisines}</h4>
+      </div>
+      <a href={`/places/${data.id}/edit`} class="btn btn-warning">
+        Edit
+      </a>
+      <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        <button type="submit" class="btn btn-danger">
+          Delete
+        </button>
+      </form>
+    </div>
+  </div>
+  <div class="text-center">
+    <h2>Comments</h2>
+    <p>No comments yet!</p>
+  </div>
+</main>
+
     </Def>
   );
 }
